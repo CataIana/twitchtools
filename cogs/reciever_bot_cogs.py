c20e2c4d7887e6f8bfa2fa99d070d9a8a531c91b
CataIana <kara_m@outlook.com.au>
2022-02-02 14:39:05 +1100

    Fix bot owners not overriding permissions for manager role

--- cogs/reciever_bot_cogs.py
+++ cogs/reciever_bot_cogs.py
@@ -59,6 +59,36 @@ def has_manage_permissions() -> Callable[[T], T]:
 
     return commands.check(predicate)
 
+def has_guild_permissions(owner_override: bool = False, **perms: bool) -> Callable[[T], T]:
+    """Similar to :func:`.has_permissions`, but operates on guild wide
+    permissions instead of the current channel permissions.
+    If this check is called in a DM context, it will raise an
+    exception, :exc:`.NoPrivateMessage`.
+    .. versionadded:: 1.3
+    """
+
+    invalid = set(perms) - set(disnake.Permissions.VALID_FLAGS)
+    if invalid:
+        raise TypeError(f"Invalid permission(s): {', '.join(invalid)}")
+
+    async def predicate(ctx: ApplicationCustomContext) -> bool:
+        if not ctx.guild:
+            raise commands.NoPrivateMessage
+
+        if owner_override:
+            if await ctx.bot.is_owner(ctx.author):
+                return True
+
+        permissions = ctx.author.guild_permissions  # type: ignore
+        missing = [perm for perm, value in perms.items() if getattr(permissions, perm) != value]
+
+        if not missing:
+            return True
+
+        raise commands.MissingPermissions(missing)
+
+    return commands.check(predicate)
+
 class RecieverCommands(commands.Cog):
     def __init__(self, bot):
         self.bot: TwitchCallBackBot = bot
@@ -253,7 +283,7 @@ class RecieverCommands(commands.Cog):
                         pass
 
     @commands.slash_command(name="managerrole")
-    @commands.has_guild_permissions(administrator=True)
+    @has_guild_permissions(owner_override=True, administrator=True)
     async def manager_role(self, ctx: ApplicationCustomContext):
         pass
 
